a80ebfb102146b70987ba8f5fa9c3e0d83252d92
Vova12323 <68323917+Vova12323@users.noreply.github.com>
2022-04-01 11:03:18 +0900

    Add files via upload

---
+++ control.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class control : MonoBehaviour
+{
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.mass = 1000;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(0, 0, 250f);
+        
+    }
+    void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+    
+}
